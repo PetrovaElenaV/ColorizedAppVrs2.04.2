db5672e48345c8f2c6d783c77e020005a24a3642
Elena Petrova <e_v_b@mail.ru>
2023-08-11 23:43:25 +0300

    Initial Commit

---
+++ ColorizedAppVrs2.04.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedAppVrs2.04.2
+//
+//  Created by Elena Petrova on 11.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
