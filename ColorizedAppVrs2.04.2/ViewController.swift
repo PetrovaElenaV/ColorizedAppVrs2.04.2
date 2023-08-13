3447e224b9078a93e528dcc0424e651ca18bedfe
Elena Petrova <e_v_b@mail.ru>
2023-08-13 14:38:57 +0300

    прога работает , но нужно проработать с кострейнтами

--- ColorizedAppVrs2.04.2/ViewController.swift
+++ ColorizedAppVrs2.04.2/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var colorRGBView: UIView!
+    
+    @IBOutlet var numberOfRedLabel: UILabel!
+    @IBOutlet var numberOfGreenLabel: UILabel!
+    @IBOutlet var numberOfBlueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupRedSlider()
+        setupBlueSlider()
+        setupGreenSlider()
+        
+        redSliderAction()
+        greenSliderAction()
+        blueSliderAction()
     }
-
-
+    // MARK: - IBActions
+    
+    @IBAction func redSliderAction() {
+        numberOfRedLabel.text = String(format:"%.2f", redSlider.value)
+        colorRBG()
+    }
+    
+    @IBAction func greenSliderAction() {
+        numberOfGreenLabel.text = String(format: "%0.2f", greenSlider.value)
+        colorRBG()
+    }
+    
+    @IBAction func blueSliderAction() {
+        numberOfBlueLabel.text = String(format: "%0.2f", blueSlider.value)
+        colorRBG()
+    }
+    
+    // MARK: - Private Slider
+    
+    private func setupRedSlider() {
+        redSlider.value = 0.05
+        redSlider.minimumTrackTintColor = .red
+    }
+    private func setupGreenSlider() {
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.value = 0.27
+    }
+    private func setupBlueSlider() {
+        blueSlider.value = 0.49
+    }
+    private func colorRBG() {
+        colorRGBView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 }
-
